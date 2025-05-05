7780876cbd7059303ac40507b1c3dea5fa66583b
nazwan74 <muhnazwanfadhilah74@gmail.com>
2025-05-05 15:00:26 +0700

    menambah fitur serch dan fitur filter di laporan

--- resources/views/admin/laporan.blade.php
+++ resources/views/admin/laporan.blade.php
@@ -252,6 +252,6 @@
                 categoryFilter.addEventListener('change', filterTable);
             });
         </script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
