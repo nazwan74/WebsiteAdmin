a711c0ee85edf178922ff2d58cdc0e346419b368
nazwan74 <muhnazwanfadhilah74@gmail.com>
2026-04-21 13:32:15 +0700

    menyesuaikan warna tema

--- app/Http/Middleware/AdminNoCache.php
+++ app/Http/Middleware/AdminNoCache.php
@@ -18,8 +18,10 @@ class AdminNoCache
         $response = $next($request);
 
         // Tambahkan header untuk melarang browser menyimpan cache halaman admin
-        return $response->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
-                        ->header('Pragma', 'no-cache')
-                        ->header('Expires', 'Sun, 02 Jan 1990 00:00:00 GMT');
+        $response->headers->set('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', 'Sun, 02 Jan 1990 00:00:00 GMT');
+
+        return $response;
     }
 }
