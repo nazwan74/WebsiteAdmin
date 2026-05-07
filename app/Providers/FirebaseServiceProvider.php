4ca052ae6aee2a0949ebd0202ea5c6e4b5aec8f2
nazwan74 <muhnazwanfadhilah74@gmail.com>
2026-05-07 21:11:37 +0700

    mengubah credential

--- app/Providers/FirebaseServiceProvider.php
+++ app/Providers/FirebaseServiceProvider.php
@@ -12,27 +12,38 @@ class FirebaseServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $credentialsPath = config('firebase.credentials');
+
+        if ($credentialsPath && file_exists($credentialsPath)) {
+            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $credentialsPath);
+        }
+
         // Singleton untuk Firestore
-        $this->app->singleton('firebase.firestore', function ($app) {
-            return (new Factory)
-                ->withServiceAccount(config('firebase.credentials'))
-                ->createFirestore()
-                ->database();
+        $this->app->singleton('firebase.firestore', function ($app) use ($credentialsPath) {
+            $factory = (new Factory);
+            if ($credentialsPath && file_exists($credentialsPath)) {
+                $factory = $factory->withServiceAccount($credentialsPath);
+            }
+            return $factory->createFirestore()->database();
         });
 
         // Singleton untuk Storage
-        $this->app->singleton('firebase.storage', function ($app) {
-            return (new Factory)
-                ->withServiceAccount(config('firebase.credentials'))
-                ->withDefaultStorageBucket(config('firebase.storage_bucket'))
+        $this->app->singleton('firebase.storage', function ($app) use ($credentialsPath) {
+            $factory = (new Factory);
+            if ($credentialsPath && file_exists($credentialsPath)) {
+                $factory = $factory->withServiceAccount($credentialsPath);
+            }
+            return $factory->withDefaultStorageBucket(config('firebase.storage_bucket'))
                 ->createStorage();
         });
 
         // Singleton untuk Auth
-        $this->app->singleton('firebase.auth', function ($app) {
-            return (new Factory)
-                ->withServiceAccount(config('firebase.credentials'))
-                ->createAuth();
+        $this->app->singleton('firebase.auth', function ($app) use ($credentialsPath) {
+            $factory = (new Factory);
+            if ($credentialsPath && file_exists($credentialsPath)) {
+                $factory = $factory->withServiceAccount($credentialsPath);
+            }
+            return $factory->createAuth();
         });
     }
 
